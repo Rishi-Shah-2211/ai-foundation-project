9861a35c5bc3ce2807fe8aa4d8fb5b24856dde75
Rishi Shah <rishishah2211@gmail.com>
2026-02-28 05:07:55 +0530

    Added streamlit app and requirements file

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Rishi")
+import streamlit as st
+
+st.title("AI Engineer Foundation 🚀")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.success(f"Welcome to your AI journey, {name}!")
